f702f64cd6165b7df1d3ac7306015340aac62345
SimonPhiDang <201705957@post.au.dk>
2021-02-04 13:17:08 +0100

    Calculator løsning

---
+++ Calculator.Unit.Test/CalculatorTestv2.cs
@@ -0,0 +1,75 @@
+using NUnit.Framework;
+
+
+namespace Calculator.Test.Unit
+{
+	[TestFixture]
+	public class CalculatorTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void AdditionTest_PositiveInteger_Equal()
+		{
+			var calc = new calculator();
+			var result = calc.Add(3, 5);
+			Assert.AreEqual(8,result);
+		}
+
+		[Test]
+		public void AdditionTest_NegativeInteger_NotEqual()
+		{
+			var calc = new calculator();
+			var calculated = calc.Add(-4, -4);
+			Assert.AreNotEqual(8,calculated);
+		}
+
+
+		[Test]
+		public void SubtractionTest_PositiveInteger_Equal()
+		{
+			var calc = new calculator();
+			var result = calc.Subtract(5, 1);
+			Assert.AreEqual(4,result);
+		}
+
+		[Test]
+		public void SubtractionTest_NegativeInteger_Equal()
+		{
+			var calc = new calculator();
+			var result = calc.Subtract(-5, -2);
+			Assert.AreEqual(-3, result);
+		}
+
+		[TestCase(1,4,4)]
+		[TestCase(2, 1, 2)]
+		[TestCase(6, 3, 18)]
+		public void MultiplyTest_PositiveInteger_Equal(double first, double second, double expectedresult)
+		{
+			var calc = new calculator();
+			var calculated = calc.Multiply(first, second);
+			Assert.AreEqual(expectedresult,calculated);
+		}
+
+		[Test]
+		public void PowerTest_PositiveInteger_EqualTrue()
+		{
+			var calc = new calculator();
+			var calculated = calc.Power(4, 2);
+			Assert.AreEqual(16,calculated);
+		}
+
+        [Test]
+        public void Power_NegativeInteger_EqualTrue()
+        {	
+            var uut = new calculator();
+            
+            var ans = uut.Power(-2, 2);
+
+            Assert.That(4, Is.EqualTo(ans));
+        }
+	}
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Calculator
+{
+	public class calculator
+	{
+		static void Main(string[] args)
+		{
+			calculator cal = new calculator();
+
+			double a = 10;
+			double b = 5;
+
+			double result;
+
+			result = cal.Add(a, b);
+			Console.WriteLine("Add result: " + result);
+
+			result = cal.Subtract(a, b);
+			Console.WriteLine("Subtract result: " + result);
+
+			result = cal.Multiply(a, b);
+			Console.WriteLine("Multiply result: " + result);
+
+			result = cal.Power(a, b);
+			Console.WriteLine("Power result: " + result);
+
+		}
+
+
+		public double Add(double a, double b)
+		{
+			return a + b;
+		}
+
+		public double Subtract(double a, double b)
+		{
+			return a - b;
+		}
+
+		public double Multiply(double a, double b)
+		{
+			return a * b;
+		}
+
+		public double Power(double x, double exp)
+		{
+			return Math.Pow(x, exp);
+		}
+
+	}
+
+}
